edae5c9a8d44031a8bdb1d0b54330bc5e1500dbc
dinikoff <zarazablin@gmail.com>
2022-11-20 13:12:13 +0300

    step 5 - fix linter errors

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -16,7 +16,8 @@ def game_engine(game):
             print('Correct!')
             count += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
     else:
