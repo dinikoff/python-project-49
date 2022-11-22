5cfd8949032ca8f2200b2bde5d99e11c25922550
dinikoff <zarazablin@gmail.com>
2022-11-22 19:55:43 +0300

    refactor - delete magic number

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,6 +2,7 @@ import random
 
 MAX_NUMBER = 99
 MIN_NUMBER = 1
+OPERATION_NUMBER = 3
 
 
 def get_welcome_message():
@@ -11,7 +12,7 @@ def get_welcome_message():
 def get_game_data():
     first_number = random.randint(MIN_NUMBER, MAX_NUMBER)
     second_number = random.randint(MIN_NUMBER, MAX_NUMBER)
-    operation = ('+', '-', '*')[random.randint(0, 2)]
+    operation = ('+', '-', '*')[random.randint(0, OPERATION_NUMBER - 1)]
     question = f'{first_number} {operation} {second_number}'
     if operation == '+':
         answer = first_number + second_number
